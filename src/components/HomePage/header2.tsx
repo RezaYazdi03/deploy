f9ac0ae52a9844c898f4e57ece948c6dfdaefc4f
RezaYazdi03 <yazdireza2003@gmail.com>
2025-05-25 21:01:04 +0330

    sprint5

--- src/components/HomePage/header2.tsx
+++ src/components/HomePage/header2.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import Brightness2OutlinedIcon from '@mui/icons-material/Brightness2Outlined';
@@ -14,7 +16,12 @@ import { useTheme } from "../theme";
 import LoginModal from "./login/login";
 import dynamic from "next/dynamic";
 import Link from "next/link";
-
+import { useADDRESS } from "../../context/GetAddress";
+import {  
+  FaChevronDown,
+  FaChevronLeft,
+} from "react-icons/fa";
+import AddressModal from "../../app/OrderSubmission/AddressModal/AddressModal";
 const LazySearch = dynamic(() => import('./search'), {
     loading: () => (
         <div className="flex items-center rounded-3xl p-2 dark:bg-[#383535] bg-[#D9D9D9] mx-auto " dir='rtl'>
@@ -31,12 +38,20 @@ const LazySearch = dynamic(() => import('./search'), {
 });
 
 export default function Header2() {
+    const [isMapOpen,SetIsMapOpen]=useState(false);
     const { isDarkMode, toggleDarkMode } = useTheme();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [shoppingNum, setShoppingNum] = useState(2);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpenAddress, setModalOpenAddress] = useState(false);
+    const { data = [] } = useADDRESS();  
+    const selected = data?.find((add) => add.isChosen === true);
+    const OpenMap=()=>setModalOpenAddress(true);
+    const handleCloseModalAddress = () => {
+        setModalOpenAddress(false);
+    };
     const getUsername = async () => {
-        // localStorage.removeItem('token');  
+        //localStorage.removeItem('token');  
         const token = localStorage.getItem("token");
         console.log('Retrieved Token:', token);
 
@@ -64,10 +79,15 @@ export default function Header2() {
     };
     const handleOpenModal = () => {
         setIsModalOpen(true);
-    };
+        handleCloseModalAddress();    };
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
+    };
+    const truncateWords = (text, wordLimit = 4) => {
+        if (!text) return "...";
+        const words = text.split(" ");
+        return words.length > wordLimit ? words.slice(0, wordLimit).join(" ") + "...": text;
     };
 
     useEffect(() => {
@@ -81,7 +101,7 @@ export default function Header2() {
             <div className={` dark:bg-[#191919] bg-white  w-full`}>
                 <div dir="rtl" className="flex flex-col justify-center px-5 py-2 md:flex-row">
 
-                    <div className="hidden md:flex basis-2/10 my-auto">
+                    <div className="hidden md:flex basis-1/10 my-auto">
                         <Link href="/">
                             <Image src={isDarkMode ? `/assets/logo-dark.png` : `/assets/logo.png`}
                                 alt="Logo"
@@ -90,7 +110,16 @@ export default function Header2() {
                                 className="mx-auto md:mr-0 md:ml-auto md:w-30 md:h-20" />
                         </Link>
                     </div>
-
+                    <div className="flex flex-col mt-4 ml-20 leading-relaxed cursor-pointer" onClick={OpenMap}>
+                        <span className="text-md text-black font-bold mr-5">{selected?.name}</span>
+                        <div className="flex flex-row-reverse gap-1">
+                            <FaChevronDown className="text-[#f18825] h-3 w-3 mt-1"/>
+                            <span className="font-medium text-sm text-gray-500 truncate">
+                                {truncateWords(selected?.address)}
+                            </span>  
+                            <IoLocationOutline/>
+                        </div>             
+                    </div>
                     <div className="basis-5/10 w-full my-10 md:my-auto mx-auto ">
                         <LazySearch isDarkMode={isDarkMode} />
                     </div>
@@ -98,7 +127,7 @@ export default function Header2() {
                     <div dir="ltr" className={`hidden basis-3/10 md:flex my-auto gap-10 dark:text-white text-black`}>
                         {isLoggedIn ? (
                             <Link
-                                href="/ProfilePage/OrdersPage"
+                                href="/ProfilePage"
                                 className="text-[15px] cursor-pointer"
 
                             >
@@ -135,7 +164,7 @@ export default function Header2() {
                '>
                 {isLoggedIn ? (
                     <Link
-                        href="/ProfilePage/OrdersPage"
+                        href="/ProfilePage"
                         className='
                          text-black dark:text-white bg-orange-400
                          rounded-2xl p-3 cursor-pointer
@@ -168,6 +197,7 @@ export default function Header2() {
                 </Link>
             </div>
             {isModalOpen && <LoginModal onClose={handleCloseModal} open={isModalOpen} setIsLoggedIn={setIsLoggedIn} />}
+            {isModalOpenAddress && <AddressModal onClose={handleCloseModalAddress} id_user={localStorage.getItem("token")}/>}
         </>
     );
 }
